d2c42126ec100f8e95b18f8c7ffa271d64c43a8d
兜小神 <qian.yang@tianla.lu>
2017-10-18 10:31:42 +0800

    no message

--- Helios.Orders/Helios.Orders.Tests/Class1.cs
+++ Helios.Orders/Helios.Orders.Tests/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xunit;
 
 namespace Helios.Orders.Tests
 {
@@ -16,7 +17,21 @@ namespace Helios.Orders.Tests
      * 
      * 
      */
-    public class Class1
+    public class XTestDemo
     {
+        [Fact(DisplayName = "单元测试示例")]
+        public void Test1()
+        {
+            Assert.Equal(2, 1+1);
+        }
+
+        [Theory(DisplayName = "多参数单元测试示例")]
+        [InlineData(1, 2, 3)]
+        [InlineData(78, 34, 2652)]
+        [InlineData(78, 34, 100)]
+        public void Test2(int a, int b, int result)
+        {
+            Assert.Equal(result, a * b);
+        }
     }
 }
