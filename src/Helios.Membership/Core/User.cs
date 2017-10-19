cdb68411ccfbab3a68705125aaba46647a3ffff0
兜小神 <qian.yang@tianla.lu>
2017-10-19 12:44:43 +0800

    no message

---
+++ src/Helios.Membership/Core/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Abp.Authorization.Users;
+
+// ReSharper disable once CheckNamespace
+namespace Helios.Membership
+{
+    public class User : AbpUser<User>
+    {
+        public const string DefaultPassword = "123qwe";
+        public const string EmptyPassword = "[EMPTY]";
+        public const int MaxPhoneNumberLength = 32;
+    }
+}
